fc35fc6e2d15215b6c32cdbe286c0a6ed2644411
BEB03 <cpt9136@gmail.com>
2022-05-10 21:45:02 +0900

    temp

--- contract/migrations/1_initial_migration.js
+++ contract/migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 const Migrations = artifacts.require("./Migrations.sol");
-const HelloWorld = artifacts.require("./HelloWorld.sol");
+const Exchange = artifacts.require("./Exchange.sol");
+const Token = artifacts.require("./Token.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(HelloWorld);
+  deployer.deploy(Exchange, "0x809c8c6444c43e99A72efFD5Cd98e3B76c2AD8F2");
+  deployer.deploy(Token, "testcoin", "TST", 13, 1000000000000000); // 인자에 name, symbol, demical, initialSupply 작성
 };
