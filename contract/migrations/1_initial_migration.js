cd9f3f9329d3129b93afc06cf20592fb51d546ab
BEB03 <cpt9136@gmail.com>
2022-05-11 00:06:42 +0900

    feat(백동현): deploy kip-7 and mint URU

--- contract/migrations/1_initial_migration.js
+++ contract/migrations/1_initial_migration.js
@@ -6,5 +6,5 @@ require("dotenv").config();
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(Exchange, process.env.TOKEN_CONTRACT);
-  // deployer.deploy(Token, "testcoin", "TST", 13, 1000000000000000); // 인자에 name, symbol, demical, initialSupply 작성
+  deployer.deploy(Token, "URUtoken", "URU", 0, 100000000);
 };
